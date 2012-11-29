ae127cf4067d6858f7739befa47e4269a6a6a782
holgerd77 <Holger.Drewes@googlemail.com>
2012-11-29 14:08:12 +0100

    Switched to temporarily sending mails from private account due to problems with official account

--- if_website/views.py
+++ if_website/views.py
@@ -36,7 +36,9 @@ def mitzeichnen(request):
             msg  = "Du hast Dich auf informationsverzicht.de mit Deiner Mitzeichnung" + "\n"
             msg += "gegen ein Leistungsschutzrecht ausgesprochen." + "\n\n"
             msg += "Zum Freischalten Deiner Mitzeichnung klicke auf folgenden Link:" +"\n"
-            msg += "http://informationsverzicht.de/freischalten/?activation_hash=" + u.activation_hash + "\n\n"
+            msg += "http://informationsverzicht.de/freischalten/?activation_hash=" + u.activation_hash + "\n\n\n"
+            msg += "Hinweis: Mail wird gerade über privaten Account versendet, "
+            msg += "da es Probleme mit dem Versand über den offiziellen Account gibt."
             
             send_mail(subject, msg, 'info@informationsverzicht.de', [u.email], fail_silently=False)
             
